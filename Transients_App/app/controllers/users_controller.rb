9e7b4fbfa02b46ba7f86f09891848ccac07adb2c
Zachary Totta <ztotta@gmail.com>
2016-10-30 20:25:51 -0700

    user profile edit/update paths linked through modal

--- Transients_App/app/controllers/users_controller.rb
+++ Transients_App/app/controllers/users_controller.rb
@@ -23,7 +23,21 @@ class UsersController < ApplicationController
   def index
       @users = User.all
   end
+    
+  def edit    
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
 
+    if @user.update(user_params)
+      redirect_to user_path(@user)
+    else
+      render :edit
+    end
+  end
+      
   private
     def user_params
       params.require(:user).permit(:username, :email, :password, :password_confirmation)
