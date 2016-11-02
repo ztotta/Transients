7ea52dc6681364d8870755642d34a652246ac01e
Zachary Totta <ztotta@gmail.com>
2016-11-02 16:30:55 -0700

    update environments

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,17 +1,19 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  config.paperclip_defaults = {
-  :storage => :s3,
-  :s3_host_name => 's3-us-west-2.amazonaws.com',
-  :s3_credentials => {
-    :bucket => ENV['S3_BUCKET_NAME'],
-    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-  },
+config.paperclip_defaults = {
+    storage: :s3,
+    s3_protocol: :https,
+    :s3_host_name => 's3-us-west-2.amazonaws.com',
+    s3_credentials: {
+      bucket: ENV.fetch('S3_BUCKET_NAME'),
+      access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
+      secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+      s3_region: ENV.fetch('AWS_REGION'),
+    },
     :url =>':s3_domain_url',
     :path => '/:class/:attachment/:id_partition/:style/:filename',
-}
+  }
 
   config.serve_static_files = true
   config.assets.compile = true    
