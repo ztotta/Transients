fa4baace981dd19828e3f9923f9f3c3820d5a72b
Zachary Totta <ztotta@gmail.com>
2016-11-02 13:43:14 -0700

    waveform display and jquery <tr> fadeout

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,13 +16,62 @@
 
 //= require bootstrap
 //= require_tree .
-setTimeout(function(){ 
-    console.log('scirp');
-    console.log($('#track-list .song-row').length);
-    if ($('#track-list .song-row').length >= 20 ) {
-        $('body').append("<p id='load-more'><span>LOAD MORE</span></p>");
-    }
-}, 1000);
+ 
+console.log('scirp');
+console.log($('#track-list .song-row').length);
 
-    
+if ($('#track-list .song-row').length >= 20 ) {
+    $('body').append("<p id='load-more'><span>LOAD MORE</span></p>");
+}
+if ($('#users-index .user-image').length >= 20 ) {
+    $('body').append("<p id='load-more'><span>LOAD MORE</span></p>");
+}
+
+// wavesurfer dance
+
+var waveCounter = 0;
+var wavez = "wavesurfer"+waveCounter
+
+$('.song-row').click(function(){
+    $(this).fadeOut(100);
+    $('.footer').html(
+        `<div id="wave-counting" >
+            <progress id="progress" class="progress progress-striped" value="100" max="100" style="display: none;"></progress>
+            <wave style="position: absolute; z-index: 2; left: 0px; top: 0px; bottom: 0px; overflow: hidden; width: 0px; display: block; box-sizing: border-box; border-right: 1px solid rgb(255, 255, 255);">
+                <canvas width="1820" height="256" style="width: 910px;"></canvas>
+            </wave>
+        </div>`);
+    $('#wave-counting').attr("id", "waveform" + waveCounter);
+    var wavesurfer = WaveSurfer.create({
+            container: ('#waveform' + waveCounter),
+            waveColor: 'violet',
+            progressColor: 'purple',
+            height: 37
+    });
+    wavesurfer.load('http://ia902606.us.archive.org/35/items/shortpoetry_047_librivox/song_cjrg_teasdale_64kb.mp3');
+    wavecounter++
+})
+
+
+
+
+
+//var wavesurfer = WaveSurfer.create({
+//    container: ('#waveform' + wavecounter),
+//    waveColor: 'violet',
+//    progressColor: 'purple',
+//    height: 37
+//});
+// increment 
+
+//wavesurfer.load('http://ia902606.us.archive.org/35/items/shortpoetry_047_librivox/song_cjrg_teasdale_64kb.mp3');
+
+
+
+//  on click: 
+//      replace '.song-row' HTML with waveform html (store in var, include td-colspan=7)
+//          play/pause button only
+//      wavesurfer+i.load('#url#')
+//      wavesurfer+i.play
+
 
---
+++ app/assets/javascripts/tracks.js
@@ -0,0 +1,7 @@
+setTimeout(function(){ 
+    console.log('scirp');
+    console.log($('#track-list .song-row').length);
+    if ($('#track-list .song-row').length >= 20 ) {
+        $('body').append("<p id='load-more'><span>LOAD MORE</span></p>");
+    }
+}, 1000);
