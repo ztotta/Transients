f134cf750c310b3f88acb8dc2dfe42b70fcbd035
Zachary Totta <ztotta@gmail.com>
2016-11-03 21:08:19 -0700

    update migrations: just add (no remove) length_select column

--- db/migrate/20161104040357_add_length_select_to_track2.rb
+++ db/migrate/20161104040357_add_length_select_to_track2.rb
@@ -1,6 +1,6 @@
 class AddLengthSelectToTrack2 < ActiveRecord::Migration
   def change
-      remove_column :tracks, :length_select, :string
+#      remove_column :tracks, :length_select, :string
       add_column :tracks, :length_select, :string
   end
 end
